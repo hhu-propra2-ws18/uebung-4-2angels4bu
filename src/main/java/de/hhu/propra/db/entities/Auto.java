0806a22131a127c2e4dd8a2d6af302bc5996fbbb
macrimo <42834598+macrimo@users.noreply.github.com>
2018-11-15 14:46:49 +0100

    hab jetzt mal paar attribute und annotationen hinzugefügt

--- src/main/java/de/hhu/propra/db/entities/Auto.java
+++ src/main/java/de/hhu/propra/db/entities/Auto.java
@@ -1,5 +1,22 @@
 package de.hhu.propra.db.entities;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
 public class Auto{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private Kunde besitzer;
+
+    private String farbe;
+    private String marke;
+    private boolean hubraum;
 
 }
